60f6fdb5fe1d21b0b26de4dd312eb7bf7cce29c7
harshthakur066 <harshthakur066@gmail.com>
2020-04-24 23:57:17 +0530

    api fetched with state management

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,21 @@
-import Layout from "../components/layout";
+import Fetch from "isomorphic-unfetch";
 
-const Index = () => (
+import Layout from "../components/layout";
+import Prices from "../components/prices";
+
+const Index = ({ bpi }) => (
   <Layout>
     <div>
       <h1>Welcome to BitzPrice</h1>
+      <Prices bpi={bpi} />
     </div>
   </Layout>
 );
-
+Index.getInitialProps = async () => {
+  const res = await Fetch("https://api.coindesk.com/v1/bpi/currentprice.json");
+  const data = await res.json();
+  return {
+    bpi: data.bpi,
+  };
+};
 export default Index;
